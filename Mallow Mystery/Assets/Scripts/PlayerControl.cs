84f3fd8bdb388548894acb78f9b8dba7287b4d52
Robin Suurs <17059143@student.hhs.nl>
2023-09-18 12:01:50 +0200

    added basic movement

---
+++ Mallow Mystery/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Vector2 _movement;
+    [SerializeField] private float speed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(_movement.x,0,_movement.y)* (speed*Time.deltaTime));
+    }
+
+    void OnMove(InputValue inputValue)
+    {
+        _movement = inputValue.Get<Vector2>();
+    }
+}
