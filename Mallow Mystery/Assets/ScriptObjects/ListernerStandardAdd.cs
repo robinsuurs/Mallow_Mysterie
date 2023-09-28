de35e68052459eb526a36bb501416e5e801a4d5d
Zizoraf <mulders71@gmail.com>
2023-09-28 14:05:40 +0200

    Created an Dialogue UI and Standardized Event

---
+++ Mallow Mystery/Assets/ScriptObjects/ListernerStandardAdd.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using ExampleEventScriptAble;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ListernerStandardAdd : MonoBehaviour
+{
+
+    public GameEventStandardAdd Event;
+    public UnityEvent response;
+
+
+    private void OnEnable()
+    {
+        Event.AddListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.RemoveListener(this);
+    }
+
+    public void OnEventTriggered()
+    {
+        print("Invoked");
+        response.Invoke();
+    }
+}
