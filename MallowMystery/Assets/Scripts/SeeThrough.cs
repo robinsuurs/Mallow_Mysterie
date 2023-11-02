cf0f9b08f7d1e24b353913a268812d9889fe9cf7
Robin Suurs <17059143@student.hhs.nl>
2023-11-02 20:20:36 +0100

    Progress!!!
    Started coding my own sharder in code.
    To do:
    add position input from c# script
    Make the circle out put a STEP into the alpha channel

--- MallowMystery/Assets/Scripts/SeeThrough.cs
+++ MallowMystery/Assets/Scripts/SeeThrough.cs
@@ -18,22 +18,23 @@ public class SeeThrough : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Vector2 cutoutPos = _mainCamera.WorldToViewportPoint(targetObject.position);
-        cutoutPos.y /= (Screen.width / Screen.height);
+        // Vector2 cutoutPos = _mainCamera.WorldToViewportPoint(targetObject.position);
+        // cutoutPos.y /= (Screen.width / Screen.height);
 
-        Vector3 offset = targetObject.position - transform.position;
-        RaycastHit[] hits = Physics.RaycastAll(transform.position, offset, offset.magnitude, layerMask);
-
-        foreach (var t in hits)
+        // Vector3 offset = targetObject.position - transform.position;
+        // RaycastHit[] hits = Physics.RaycastAll(transform.position, offset, offset.magnitude, layerMask);
+        if (Physics.Linecast(transform.position, targetObject.position, out RaycastHit hitInfo,layerMask))
         {
-            var materials = t.transform.GetComponent<Renderer>().materials;
+            
+            var materials = targetObject.transform.GetComponent<Renderer>().materials;
 
             for(int j = 0; j < materials.Length; j++)
             {
-                materials[j].SetVector("_CutoutPos", cutoutPos);
+                materials[j].SetVector("_CutoutPos", hitInfo.textureCoord);
                 materials[j].SetFloat("_CutoutSize", 0.1f);
                 materials[j].SetFloat("_FalloffSize", 0.05f);
             }
-        }
+        } 
     }
 }
+
