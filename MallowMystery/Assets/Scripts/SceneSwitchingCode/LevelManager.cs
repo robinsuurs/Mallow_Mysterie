2a85f4793028e1dfd29d9ffe3c2f189e3690d98a
Zizoraf <mulders71@gmail.com>
2023-11-02 19:54:36 +0100

    A shit ton of ducttape
    .

--- MallowMystery/Assets/Scripts/SceneSwitchingCode/LevelManager.cs
+++ MallowMystery/Assets/Scripts/SceneSwitchingCode/LevelManager.cs
@@ -12,6 +12,7 @@ using UnityEngine.Serialization;
 public class LevelManager : ScriptableObject {
     public SceneSwitchData sceneSwitchData;
     [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private GameObject playerOverworld;
     
 
     public void SpawnPlayer(GameData gameData) {
@@ -19,7 +20,8 @@ public class LevelManager : ScriptableObject {
         List<GameObject> spawnLocations = GameObject.FindGameObjectsWithTag("SpawnLocation").ToList();
         if (sceneSwitchData != null) {
             foreach (var spawn in spawnLocations.Where(spawn => spawn.name.Equals(sceneSwitchData.playerSpawnLocationName))) {
-                Instantiate(playerPrefab, spawn.gameObject.transform.position, quaternion.identity);
+                instantiate(spawn.gameObject.transform.position);
+                // Instantiate(playerPrefab, spawn.gameObject.transform.position, quaternion.identity);
                 spawnedPlayer = true;
                 break;
             }
@@ -28,20 +30,23 @@ public class LevelManager : ScriptableObject {
             if (!gameData.sceneName.Equals(SceneManager.GetActiveScene().name)) {
                 if (spawnLocations.Exists(spawn => spawn.name.Equals("TestSpawn"))) {
                     foreach (var spawn in spawnLocations.Where(spawn => spawn.name.Equals("TestSpawn"))) {
-                        Instantiate(playerPrefab, spawn.transform.position, quaternion.identity); //This is for Testing purposes
+                        instantiate(spawn.transform.position);
+                        // Instantiate(playerPrefab, spawn.transform.position, quaternion.identity); //This is for Testing purposes
                         spawnedPlayer = true;
                         break;
                     }
                 }
             } else {
-                Instantiate(playerPrefab, gameData.playerLocation, quaternion.identity);
+                instantiate(gameData.playerLocation);
+                // Instantiate(playerPrefab, gameData.playerLocation, quaternion.identity);
                 spawnedPlayer = true;
             }
         }
 
         if (spawnedPlayer) return;
         Debug.Log("No spawn location found, spawning player at 0,0,0");
-        Instantiate(playerPrefab, new Vector3(0,0,0), quaternion.identity);
+        instantiate(new Vector3(0,0,0));
+        // Instantiate(playerPrefab, new Vector3(0,0,0), quaternion.identity);
     }
     
     //TODO BM: 29-10-2023 Look into async load of scene, has to be in monobehaviour because of coroutine
@@ -52,5 +57,14 @@ public class LevelManager : ScriptableObject {
         DataPersistenceManager.instance.SaveGame();
         SceneManager.LoadScene(sceneSwitchData.sceneName);
         Time.timeScale = 1;
+    }
+    
+    private void instantiate(Vector3 loc) {
+        if (SceneManager.GetActiveScene().name.Equals("OverworldMap")) {
+            Instantiate(playerOverworld, loc, quaternion.identity);
+        }
+        else {
+            Instantiate(playerPrefab, loc, quaternion.identity);
+        }
     }
 }
