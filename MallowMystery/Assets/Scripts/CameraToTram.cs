ef95e3e3a808853bc89de887474c0527871030ac
Zizoraf <mulders71@gmail.com>
2023-12-05 15:22:56 +0100

    fixed mistake

--- MallowMystery/Assets/Scripts/CameraToTram.cs
+++ MallowMystery/Assets/Scripts/CameraToTram.cs
@@ -21,7 +21,7 @@ public class CameraToTram : MonoBehaviour, IDataPersistence {
         // if (cameraHasPanned) return;
         
         _inputAction.Disable();
-        mainCam = mainCam;
+        mainCam = Camera.main;
         
         orignalLocation = mainCam.transform.position;
 
