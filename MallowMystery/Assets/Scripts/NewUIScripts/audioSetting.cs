03aff959d2d886424d005026f505c740f3f2fa09
Zizoraf <mulders71@gmail.com>
2023-12-18 15:34:35 +0100

    fixed soundsettings in UI

--- MallowMystery/Assets/Scripts/NewUIScripts/audioSetting.cs
+++ MallowMystery/Assets/Scripts/NewUIScripts/audioSetting.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
 
 public class audioSetting : MonoBehaviour
 {
     [SerializeField] private Slider slider;
     [SerializeField] private TMP_Text handleText;
+    [SerializeField] private AudioMixer audioMixer;
 
     private void Start()
     {
-        ValueChanged();
+        ValueChanged(PlayerPrefs.GetFloat(audioMixer.name));
     }
 
     public void increase()
@@ -26,8 +29,10 @@ public class audioSetting : MonoBehaviour
         slider.value -= 1;
     }
 
-    public void ValueChanged()
+    public void ValueChanged(float volumeLevel)
     {
         handleText.SetText(slider.value.ToString());
+        audioMixer.SetFloat(audioMixer.name, volumeLevel == 0 ? -80 : Mathf.Log10(volumeLevel/10) * 20);
+        PlayerPrefs.SetFloat(audioMixer.name, volumeLevel);
     }
 }
