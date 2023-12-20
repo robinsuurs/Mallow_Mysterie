920d0a51a3b623be5c41b242c50e39c6131303df
Zizoraf <mulders71@gmail.com>
2023-12-20 13:46:13 +0100

    made sound fadeOut that listens to ending event

--- MallowMystery/Assets/BackgroundMusicSound.cs
+++ MallowMystery/Assets/BackgroundMusicSound.cs
@@ -7,9 +7,24 @@ using UnityEngine;
 public class BackgroundMusicSound : MonoBehaviour {
     public AudioSource audioSource;
     [SerializeField] private AudioClip backgroundMusic;
+    [SerializeField] private float fadeSpeed;
 
     private void Start() {
         audioSource.clip = backgroundMusic;
         audioSource.Play();
+    }
+
+    public void soundFadeOut() {
+        StartCoroutine(soundFadeOutOverTime());
+    }
+
+    private IEnumerator soundFadeOutOverTime() {
+        float originalSoundValue = audioSource.volume;
+        float newSoundValue = 1;
+        while (audioSource.volume > 0) {
+            newSoundValue -= (fadeSpeed * Time.deltaTime);
+            audioSource.volume = (newSoundValue - (fadeSpeed * Time.deltaTime)) * originalSoundValue;
+            yield return null;
+        }
     }
 }
