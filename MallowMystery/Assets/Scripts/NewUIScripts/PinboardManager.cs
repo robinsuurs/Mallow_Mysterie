c47496e413a082e16050149b904fa50f0c76d9f9
Zizoraf <mulders71@gmail.com>
2024-01-15 10:49:47 +0100

    more things
    .

--- MallowMystery/Assets/Scripts/NewUIScripts/PinboardManager.cs
+++ MallowMystery/Assets/Scripts/NewUIScripts/PinboardManager.cs
@@ -8,12 +8,16 @@ using UnityEngine.InputSystem;
 public class PinboardManager : MonoBehaviour
 {
     [SerializeField] private GameObject pinboard;
+    [SerializeField] private List<GameObject> clues;
     [SerializeField] private GameEventStandardAdd closePinBoard;
     [SerializeField] private GameEventStandardAdd openUIElement;
     [SerializeField] private GameEventStandardAdd closeUIElement;
 
     public void openBoard(){
     	pinboard.SetActive(true);
+        foreach (GameObject clue in clues) {
+            clue.GetComponent<ItemLoadScene>().ShowObjectOrNot();
+        }
         openUIElement.Raise();
     }
     public void closeBoard(){
