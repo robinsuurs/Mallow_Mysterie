61d83d38eea57e3a4a2c65e9a88b050ffbf746c6
MrGafaji <43672660+MrGafaji@users.noreply.github.com>
2024-01-19 17:06:53 +0100

    Imported rest of animations and fixed map in poor area

--- MallowMystery/Assets/Scripts/NewUIScripts/Map_Journal.cs
+++ MallowMystery/Assets/Scripts/NewUIScripts/Map_Journal.cs
@@ -8,17 +8,23 @@ using UnityEngine.InputSystem;
 public class Map_Journal : MonoBehaviour
 {
     [SerializeField] [CanBeNull] private GameObject RenderMap;
-    [SerializeField] private GameObject ImageMap;
+    [SerializeField] private GameObject[] ImageMaps;
     [SerializeField] private RectTransform Marker;
     [SerializeField] private bool FixedMap;
+    [SerializeField] private int ImageMapIndex;
     [SerializeField] private Vector2 MarkerPosision;
 
     // Start is called before the first frame update
     void OnEnable()
     {
+        foreach (GameObject m in ImageMaps)
+        {
+            m.SetActive(false);
+        }
         if (FixedMap)
         {
-            ImageMap.SetActive(true);
+            GameObject map = ImageMaps[ImageMapIndex];
+            map.SetActive(true);
             Marker.anchoredPosition = new Vector2(MarkerPosision.x, MarkerPosision.y);
             if (RenderMap)
             {
@@ -27,7 +33,6 @@ public class Map_Journal : MonoBehaviour
         }
         else
         {
-            ImageMap.SetActive(false);
             Marker.localScale = new Vector3(0, 0, 0);
             if (RenderMap)
             {
